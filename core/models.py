fddb1879d33c256954f73e47c3b0611dfc7bdce4
anurupjana2005-aj <anurupjana2005@gmail.com>
2026-05-08 00:53:24 +0530

    updated templates

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
@@ -11,9 +12,9 @@ class Booking(models.Model):
     status = models.CharField(max_length=20, default='confirmed')
     traveler_name = models.CharField(max_length=100, blank=True)
     image_url = models.CharField(max_length=300, blank=True)
-    
+
     def __str__(self):
         return f"Booking {self.transaction_id} - {self.package_name}"
-    
+
     class Meta:
         ordering = ['-booking_date']
